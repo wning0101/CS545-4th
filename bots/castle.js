0f6b94a1e146ad69886d5e4fd7818de95d1320da
punam173 <40653998+punam173@users.noreply.github.com>
2019-02-15 12:02:10 -0800

    Add files via upload
    Added all the files

---
+++ bots/castle.js
@@ -0,0 +1,146 @@
+import { SPECS } from 'battlecode';
+import nav from './nav.js';
+
+const castle = {};
+castle.takeTurn = (self) => {
+    self.log('castle taking turn');
+   
+    const visible = self.getVisibleRobots();
+    const messagingRobots = visible.filter(robot => {
+        return robot.castle_talk != null;
+    });
+
+    const getBuildDir = () => {
+        const options = nav.offsetList.filter((direction) => nav.isPassable(
+            nav.applyDir(self.me, direction),
+            self.getPassableMap(),
+            self.getVisibleRobotMap(),
+        ));
+
+        return options[0];
+    }
+
+    messagingRobots.forEach((robot) => {
+        if (robot.castle_talk === 255) {
+            return;
+        }
+
+        const pendingPacket = self.pendingRecievedMessages[robot.id] || '';
+        if (pendingPacket.length === 0) {
+            self.pendingRecievedMessages[robot.id] = robot.castle_talk;
+        } else {
+            self.enemyCastles.push({
+                x: pendingPacket,
+                y: robot.castle_talk,
+            });
+
+            self.pendingRecievedMessages[robot.id] = null;
+        }
+    });
+
+    self.log(`Total Robots: ${messagingRobots.length}`);
+
+    const attackable = visible.filter((robot) => {
+        if (!self.isVisible(robot)){
+            return false;
+        }
+
+        const distance = (robot.x - self.me.x) ** 2 + (robot.y - self.me.y) ** 2;
+        if (
+            robot.team !== self.me.team &&
+            SPECS.UNITS[self.me.unit].ATTACK_RADIUS[0] <= distance &&
+            distance <= SPECS.UNITS[self.me.unit].ATTACK_RADIUS[1]
+        ) {
+            return true;
+        }
+
+        return false;
+    });
+
+    if (self.me.turn % 100) {
+        self.enemyCastles.forEach((castle) => {
+            const { x, y } = castle;
+            self.log(`${x}, ${y}`);
+        });
+    }
+
+    if (attackable.length > 0){
+        const robot = attackable[0];
+        const stringifiedRobot = JSON.stringify(robot);
+        const calculatedX = robot.x - self.me.x;
+        const calculatedY = robot.y - self.me.y;
+
+        self.log(stringifiedRobot);
+        self.log(`attacking! ${stringifiedRobot} at loc (${calculatedX, calculatedY})`);
+
+        return self.attack(calculatedX, calculatedY);
+    }
+
+    if (self.pilgrimsBuilt < 5 && self.karbonite >= 10) {
+        const direction = getBuildDir(self.me);
+        if (direction != null){
+            self.log(`Building a pilgrim at ${(self.me.x + 1)}, ${(self.me.y + 1)}`);
+            self.pilgrimsBuilt++;
+
+            return self.buildUnit(SPECS.PILGRIM, direction.x, direction.y);
+        }
+    } 
+
+    if (self.me.turn > 10 && self.karbonite > 30 && self.fuel > 150 ) {
+        // 
+        // const direction = getBuildDir(self.me);
+        // if (direction != null) {
+        //     let type = SPECS.PROPHET;
+
+        //     const generator = Math.floor(Math.random() * 3);
+        //     switch (generator) {
+        //         case 0:
+        //             type = SPECS.PROPHET;
+        //         case 1:
+        //             type = SPECS.PREACHER;
+        //         case 2:
+        //             self.crusaderBuilt += 1;
+        //             type = SPECS.CRUSADER;
+        //         default:
+        //             type = SPECS.PROPHET;
+        //     }
+
+        //     const id = self.buildUnit(type, direction.x, direction.y);
+        //     self.log(`----- built a unit of type ${type} with id: ${JSON.stringify(id)}`);
+        //     return id;
+        // }
+            const direction = getBuildDir();
+            
+            if(direction != null){
+                let type = SPECS.PILGRIM
+                if(self.crusadersBuilt % 5 !== 0 || self.crusadersBuilt === 0){
+                    type = SPECS.CRUSADER;
+                    self.crusadersBuilt += 1;   
+                }else if(self.prophetBuilt % 5!== 0 || self.prophetBuilt === 0) {
+                    type = SPECS.PROPHET
+                    self.prophetBuilt += 1;
+                }else if(self.preacherBuilt % 2!== 0 || self.preacherBuilt === 0){
+                    type = SPECS.PREACHER;
+                    self.preacherBuilt += 1;
+                // }else if (self.prilgrimsBuilt % 3 !== 0 || self.pilgrimsBuilt === 0){
+                //     type = SPECS.PILGRIM
+                //     self.pilgrimsBuilt += 1;              
+                }else{
+                    self.crusadersBuilt += 1;
+                    self.preacherBuilt += 1;
+                    self.prophetBuilt += 1;
+                }
+                return self.buildUnit(type, direction.x, direction.y);
+            } else {
+                self.log('direction is null');
+            }
+            
+    } else{
+        self.log(`karbonite: ${self.karbonite}`);
+        self.log(`fuel: ${self.karbonite}`);
+        self.log(`turn: ${self.me.turn}`);
+        
+    }
+};
+
+export default castle;
---
+++ bots/robot.js
@@ -0,0 +1,50 @@
+import {BCAbstractRobot, SPECS} from 'battlecode';
+import nav from './nav.js';
+import pilgrim from './pilgrim.js';
+import crusader from './crusader.js';
+import prophet from './prophet.js';
+import preacher from './preacher.js';
+import castle from './castle.js';
+
+class MyRobot extends BCAbstractRobot {
+    constructor() {
+        super();
+        this.pendingRecievedMessages = {};
+        this.enemyCastles = [];
+        this.myType = undefined;
+        this.pilgrimsBuilt = 0;
+        this.crusadersBuilt = 0;
+        this.prophetBuilt = 0;
+        this.preacherBuilt = 0;
+        this.isHoReflect = true;
+        this.mapLen = -1
+    }
+
+    turn() {
+        if (this.me.turn === 1) {
+            this.isHoReflect = nav.isHoReflect(this);
+            this.mapLen = this.map.length;
+        }
+        if (this.myType === undefined){
+            switch(this.me.unit) {
+                case SPECS.PROPHET:
+                    this.myType = prophet;
+                    break;
+                case SPECS.CASTLE:
+                    this.myType = castle;
+                    break;
+                case SPECS.PILGRIM:
+                    this.myType = pilgrim;
+                    break;
+                case SPECS.CRUSADER:
+                    this.myType = crusader;
+                    break;
+                case SPECS.PREACHER:
+                    this.myType = preacher;
+                    break;
+            }
+        }
+
+        return this.myType.takeTurn(this);
+    }
+}
